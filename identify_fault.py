d04e36846708db91a8f261b373757db13a903c11
damascenodiego <damascenodiego@usp.br>
2020-02-06 21:23:17 -0300

    identify_fault (v0.1)

---
+++ identify_fault.py
@@ -0,0 +1,132 @@
+#!/bin/python
+
+import ast
+import sys
+import re
+
+places = []
+transitions = []
+mu0 = {}
+pre = {}
+post = {}
+in_Lf = {}
+not_in_Lf = {}
+
+try:
+	fname = sys.argv[1]
+	f = open(fname,'r')
+	
+	# Read number of places and its IDs
+	f.readline().strip() # read line with 'places'
+	n_places = int(f.readline().strip())
+	for i in range(n_places):
+		line = f.readline().strip()
+		places.append(line)
+	# print(''%s'' % str(places))
+	f.readline().strip() # jump blank line
+	
+	# Read number of transitions and its IDs
+	f.readline().strip() # read line with 'transitions'
+	n_trs = int(f.readline().strip())
+	for i in range(n_trs):
+		line = f.readline().strip()
+		transitions.append(line)
+	# print(''%s'' % str(transitions))
+	f.readline().strip() # jump blank line
+
+	# Read the initial markings
+	f.readline().strip() # read line with 'mu0'
+	for i in range(n_places):
+		line = f.readline().strip()
+		mu0[places[i]] = line
+	# print(''%s'' % str(mu0))
+	f.readline().strip() # jump blank line
+
+	# Read the matrix of pre-conditions
+	f.readline().strip() # read line with 'pre'
+	for _pn in range(n_places):
+		line = f.readline().strip()
+		line = line.split(' ')
+		pre[places[_pn]] = {}
+		# print(line)
+		for _tn in range(n_trs):
+			pre[places[_pn]][transitions[_tn]] = line[_tn]
+	# print(''%s'' % str(pre))
+	f.readline().strip() # jump blank line
+
+	# Read the matrix of pre-conditions
+	f.readline().strip() # read line with 'post'
+	for _pn in range(n_places):
+		line = f.readline().strip()
+		line = line.split(' ')
+		post[places[_pn]] = {}
+		# print(line)
+		for _tn in range(n_trs):
+			post[places[_pn]][transitions[_tn]] = line[_tn]
+	# print(''%s'' % str(post))
+	f.readline().strip() # jump blank line
+
+	# Read the list of sequences in_Lf
+	f.readline().strip() # read line with 'in_Lf'
+	sz_Lf = int(f.readline().strip())
+	for _sn in range(sz_Lf):
+		_seq = f.readline().strip().split(',')
+		f.readline()
+		# _mrk = ast.literal_eval(f.readline().strip())
+		in_Lf[_sn] = _seq
+	# print('in_Lf: '%s'' % str(in_Lf))
+	f.readline().strip() # jump blank line
+	
+	# Read the list of sequences not_in_Lf
+	f.readline().strip() # read line with 'not_in_Lf'
+	sz_not_in_Lf = int(f.readline().strip())
+	for _sn in range(sz_not_in_Lf):
+		_seq = f.readline().strip().split(',')
+		f.readline()
+		# _mrk = ast.literal_eval(f.readline().strip())
+		not_in_Lf[_sn] = _seq
+	# print('not_in_Lf: '%s'' % str(not_in_Lf))
+	f.readline().strip() # jump blank line
+	f.close()
+
+	# 'Starting to print the script'
+	print('#!/bin/python')
+	print('')
+	print('from z3 import *')
+	print('')
+	print('## mu0_px is the initial marking for place px; ')
+	print('%s = %s' % (\
+		', '.join(['mu_'+places[_pn] for _pn in range(n_places)]), \
+		', '.join([mu0[places[_pn]] for _pn in range(n_places)])) \
+	)
+
+	print('')
+	print('## pi_tj is the pre-condition from place pi to transition tj')
+	for _pn in range(n_places):
+		print('%s = %s' % (\
+		', '.join([places[_pn]+'_'+transitions[_tn] for _tn in range(n_trs)]), \
+		', '.join([pre[places[_pn]][transitions[_tn]] for _tn in range(n_trs)])) \
+	)
+	print('')
+	print('## tj_pi is the post-condition from transition tj to place pi')
+	for _pn in range(n_places):
+		print('%s = %s' % (\
+		', '.join([transitions[_tn]+'_'+places[_pn] for _tn in range(n_trs)]), \
+		', '.join([post[places[_pn]][transitions[_tn]] for _tn in range(n_trs)])) \
+	)
+	print('')
+	print('## find the values for these variables')
+	for _pn in range(n_places):
+		print('f_%s, %s_f = Ints(\'f_%s %s_f\')' % (places[_pn],places[_pn],places[_pn],places[_pn]))
+
+	print('')
+	print('## \\in L^f')
+	for _sn in range(sz_Lf):
+		_counter = {transitions[_tn]:0 for _tn in range(n_trs)}
+		_counter['f'] = 0
+		
+		print(in_Lf[_sn])
+		print(_counter)
+
+except Exception as e:
+	print(e)

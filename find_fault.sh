7c83b16b3a6be43b62c4c84c84d353433c0d961b
damascenodiego <damascenodiego@gmail.com>
2020-02-10 09:51:55 -0300

    find_fault generates and runs for indexpetri0

--- find_fault.sh
+++ find_fault.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # generates the python script that uses Z3 to find the model
-python identify_fault.py indexpetri1.txt > indexpetri1.txt.py
+python identify_fault.py indexpetri0.txt > indexpetri0.txt.py
 
 # runs the python script that uses Z3 to find the model
-python indexpetri1.txt.py
+python indexpetri0.txt.py
